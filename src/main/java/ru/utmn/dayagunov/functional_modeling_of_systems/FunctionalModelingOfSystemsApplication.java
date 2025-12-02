5effff4651cdf428319dd3af047a1c2983df88e3
Yagunov Denis <spb.jagunov.denis@gmail.com>
2025-12-02 16:41:54 +0300

    initial commit

---
+++ src/main/java/ru/utmn/dayagunov/functional_modeling_of_systems/FunctionalModelingOfSystemsApplication.java
@@ -0,0 +1,14 @@
+package ru.utmn.dayagunov.functional_modeling_of_systems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FunctionalModelingOfSystemsApplication {
+
+	public static void main(String[] args) {
+        SpringApplication.run(FunctionalModelingOfSystemsApplication.class, args);
+        System.out.println("Ok");
+	}
+
+}
---
+++ src/test/java/ru/utmn/dayagunov/functional_modeling_of_systems/FunctionalModelingOfSystemsApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.utmn.dayagunov.functional_modeling_of_systems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FunctionalModelingOfSystemsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
